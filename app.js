5b58e553ec5fc27b480043a83b78a4d6e1ef5037
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-08-06 22:57:56 +0530

    Adding take pic

--- app.js
+++ app.js
@@ -72,6 +72,11 @@ app.get("/view", (req, res) => {
   });
 });
 
+// Serve the index.html file for the root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://192.168.0.116:${port}`);
 });
